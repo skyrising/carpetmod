gnembon/carpetmod112

add load chunk command (#141)
* add load chunk command

/loadchunk x z would tirgger chunk loading

* fix random shit my ide add, upsi did an xcom

carpetmodSrc/carpet/commands/CarpetCommands.java
@@ -21,6 +21,7 @@ public class CarpetCommands {
         handler.registerCommand(new CommandGrow());
         handler.registerCommand(new CommandLagSpike());
         handler.registerCommand(new CommandLight());
+        handler.registerCommand(new CommandLoadChunk());
         handler.registerCommand(new CommandLog());
         handler.registerCommand(new CommandPerimeter());
         handler.registerCommand(new CommandPing());

skyrising/carpetmod

src/main/java/carpet/commands/CarpetCommands.java
@@ -21,6 +21,7 @@ public class CarpetCommands {
         handler.method_29056(new CommandGrow());
         handler.method_29056(new CommandLagSpike());
         handler.method_29056(new CommandLight());
+        handler.method_29056(new CommandLoadChunk());
         handler.method_29056(new CommandLog());
         handler.method_29056(new CommandPerimeter());
         handler.method_29056(new CommandPing());