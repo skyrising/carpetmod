gnembon/carpetmod112

log autosave on tab screen (#138)

carpetmodSrc/carpet/utils/HUDController.java
@@ -7,7 +7,6 @@ import carpet.logging.logHelpers.PacketCounter;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.EnumDyeColor;
 import net.minecraft.network.play.server.SPacketPlayerListHeaderFooter;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Tuple;
@@ -54,6 +53,9 @@ public class HUDController
 
         player_huds.clear();
 
+        if (LoggerRegistry.__autosave)
+            log_autosave(server);
+
         if (LoggerRegistry.__tps)
             log_tps(server);
 
@@ -76,6 +78,22 @@ public class HUDController
             ((EntityPlayerMP)player).connection.sendPacket(packet);
         }
     }
+
+    private static void log_autosave(MinecraftServer server){
+        int gametick = server.getTickCounter();
+        int previous = gametick%900;
+
+        if(gametick != 0 && previous == 0) {
+            previous = 900;
+        }
+        int next = 900 - previous;
+        String color = Messenger.heatmap_color(previous,860);
+        ITextComponent[] message = new ITextComponent[]{Messenger.m(null,
+                "g Prev: ", String.format(Locale.US, "%s %d",color, previous),
+                "g  Next: ", String.format(Locale.US,"%s %d", color, next))};
+        LoggerRegistry.getLogger("autosave").log(() -> message, "Prev", previous, "Next", next);
+    }
+
     private static void log_tps(MinecraftServer server)
     {
         double MSPT = MathHelper.average(server.tickTimeArray) * 1.0E-6D;

skyrising/carpetmod

src/main/java/carpet/utils/HUDController.java
@@ -54,6 +54,9 @@ public class HUDController
 
         player_huds.clear();
 
+        if (LoggerRegistry.__autosave)
+            log_autosave(server);
+
         if (LoggerRegistry.__tps)
             log_tps(server);
 
@@ -77,6 +80,22 @@ public class HUDController
             ((ServerPlayerEntity)player).networkHandler.sendPacket(packet);
         }
     }
+
+    private static void log_autosave(MinecraftServer server){
+        int gametick = server.getTicks();
+        int previous = gametick%900;
+
+        if(gametick != 0 && previous == 0) {
+            previous = 900;
+        }
+        int next = 900 - previous;
+        String color = Messenger.heatmap_color(previous,860);
+        Text[] message = new Text[]{Messenger.m(null,
+                "g Prev: ", String.format(Locale.US, "%s %d",color, previous),
+                "g  Next: ", String.format(Locale.US,"%s %d", color, next))};
+        LoggerRegistry.getLogger("autosave").log(() -> message, "Prev", previous, "Next", next);
+    }
+
     private static void log_tps(MinecraftServer server)
     {
         double MSPT = MathHelper.average(server.lastTickLengths) * 1.0E-6D;